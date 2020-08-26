032c7480e2818e5fb3699ce3c944693a05efd923
JiangChao <explosionZing@126.com>
2020-08-26 21:35:29 +0800

    页面创建及tabBar配置

---
+++ src/pages/home/index.vue
@@ -0,0 +1,21 @@
+<template>
+  <view>Home</view>
+</template>
+
+<script>
+import { uniBadge } from "@dcloudio/uni-ui";
+export default {
+  components: { uniBadge },
+  data() {
+    return {};
+  },
+
+  created() {},
+
+  mounted() {},
+
+  methods: {}
+}; 
+</script>
+<style lang="scss" scoped>
+</style>
---
+++ src/pages/mine/index.vue
@@ -0,0 +1,32 @@
+<template>
+   <view>Mine</view>
+</template>
+
+<script>
+export default {
+
+    data() {
+        return {
+            
+        }
+    },
+
+    created() {
+
+    },
+
+
+    mounted() {
+        
+    },
+
+    methods: {
+        
+    }
+
+}
+
+</script>
+<style lang="scss" scoped>
+
+</style>
