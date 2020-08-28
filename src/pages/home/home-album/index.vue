b5dd2a4e1442756b53ceca87db262dc33c1fc717
JiangChao <explosionZing@126.com>
2020-08-28 22:39:31 +0800

    首页专辑页面列表提交

--- src/pages/home/home-album/index.vue
+++ src/pages/home/home-album/index.vue
@@ -7,6 +7,20 @@
           </swiper-item>
         </swiper>
       </view>
+      <view class="album_list">
+        <view class="album_item" v-for="item in albumList" :key="item.id">
+          <view class="album_left">
+            <image :src="item.cover"></image>
+          </view>
+          <view class="album_right">
+            <view class="album_name">{{item.name}}</view>
+            <view class="album_desc">{{item.desc}}</view>
+            <view class="album_btn">
+              <view class="album_attention"> + 关注</view>
+            </view>
+          </view>
+        </view>
+      </view>
   </view>
 </template>
 
@@ -50,6 +64,49 @@ export default {
     height: calc(750rpx/2.3);
     image {
       height: 100%;
+    }
+  }
+}
+.album_list {
+  padding:10rpx;
+  .album_item {
+    padding:10rpx 0;
+    display: flex;
+    border-bottom: 1rpx solid #eee;
+    .album_left {
+      flex: 1;
+      padding: 10rpx;
+      image {
+        height: 200rpx;
+        width: 200rpx;
+      }
+    }
+    .album_right {
+      flex: 2;
+      padding: 0 10rpx;
+      overflow: hidden;
+      .album_name {
+        font-size: 30rpx;
+        color: #000;
+        padding: 10rpx 0;
+      }
+      .album_desc {
+        font-size: 24rpx;
+        padding: 10rpx 0;
+        text-overflow: ellipsis;
+        overflow: hidden;
+        white-space: nowrap;
+      }
+      .album_btn {
+        padding: 10rpx;
+        display: flex;
+        justify-content: flex-end;
+        .album_attention {
+          color: $color;
+          border: 1rpx solid $color;
+          padding: 10rpx;
+        }
+      }
     }
   }
 }
